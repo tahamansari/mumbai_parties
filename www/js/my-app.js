45f460d5cfe5ac75dd5bfd3b3e35949f6429e4a9
Taha <taha.m.ansari@gmail.com>
2016-07-28 15:49:21 +0530

    initial commit

---
+++ www/js/my-app.js
@@ -0,0 +1,109 @@
+// Initialize app
+var myApp = new Framework7({
+
+    cache:false,
+    modalTitle:"Mumbai Parties",
+    smartSelectOpenIn:'picker',
+    swipeBackPage:false,
+
+    onAjaxStart: function (xhr) {
+
+        // myApp.alert("ajax start");
+        myApp.showIndicator();
+    },
+    onAjaxComplete: function (xhr) {
+
+        // myApp.alert("ajax complet");
+        myApp.hideIndicator();
+    }
+});
+
+var $$ = Dom7;
+
+var mainView = myApp.addView('.view-main', {
+    
+    dynamicNavbar: true
+});
+
+
+
+
+$$(document).on('deviceready', function() {
+
+    console.log("Device is ready!");
+    // console.log(myApp.params.swipePanel);
+
+
+    console.log("initial value"+Lockr.get("is_logged_in"));
+
+
+
+});
+
+
+
+myApp.onPageInit('index', function (page) {
+
+        console.log("value in index "+Lockr.get("is_logged_in"));
+
+})
+
+myApp.onPageInit('register', function (page) {
+
+    // myApp.myApp.alert('this is login page');
+
+})
+
+
+
+myApp.onPageInit('location', function (page) {
+
+    // console.log('myapp location called');
+    get_location();
+    
+    console.log("value in location "+Lockr.get("is_logged_in"));
+
+    if(Lockr.get("is_logged_in")){
+
+       $("#signin-div").html("<h2 class='username'>"+Lockr.get("name")+"</h2>");
+       $("#signout-div").css("display","block");
+
+    }
+    
+})
+
+
+// function onMapInit(map) {
+
+// }
+
+myApp.onPageInit('listview', function (page) {
+
+      get_event_type();
+
+      $("#owl-demo-listview").owlCarousel({
+
+         items : 4,
+         itemsDesktop : [1199,3],
+         itemsDesktopSmall : [979,3]
+   
+      });
+      
+})
+
+myApp.onPageInit('mapview', function (page) {
+        
+        get_initial_map_data(1);
+
+        $("#owl-demo-map").owlCarousel({
+      
+             items : 4,
+             itemsDesktop : [1199,3],
+             itemsDesktopSmall : [979,3],
+            
+   
+        });
+
+        
+
+})
---
+++ www/js/script.js
@@ -0,0 +1,34 @@
+function initilize(){
+
+
+	myApp.alert("initiliEd");
+
+}
+
+
+function check_age(age){
+
+
+	// myApp.alert($('#'+age).val());
+	var age = $('#'+age).val();
+
+	// myApp.alert(age);
+
+	if(isNaN(age)){
+
+		myApp.alert("age should be numeric");
+		$('[name="age"]').val("");
+		return false;
+	}
+
+}
+
+$(document).on('click','.tab',function(event){
+
+	$('.tab').removeClass('active-tab');
+	$(this).addClass('active-tab');
+	 
+})
+
+
+
