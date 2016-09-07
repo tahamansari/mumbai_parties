6e5a0b100d201ccae8ab9fb89786efea33d31815
ITRS-64 <itrs-64@Rachnas-MacBook-Air.local>
2016-09-07 18:17:34 +0530

    profile

--- www/js/script.js
+++ www/js/script.js
@@ -332,7 +332,7 @@ function on_success(imageURI) {
 
     var options = new FileUploadOptions();
     options.fileKey="file";
-    options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+    // options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
     options.mimeType="image/jpeg";
 
     var params = new Object();
